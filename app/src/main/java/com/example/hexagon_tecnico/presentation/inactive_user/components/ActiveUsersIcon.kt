ae0263fdf0cb69533769647e5a2ded6ea2c9703e
Pedro Borba <pedroborba005@gmail.com>
2024-04-24 18:51:11 -0300

    Retirando imports

--- app/src/main/java/com/example/hexagon_tecnico/presentation/inactive_user/components/ActiveUsersIcon.kt
+++ app/src/main/java/com/example/hexagon_tecnico/presentation/inactive_user/components/ActiveUsersIcon.kt
@@ -1,18 +1,9 @@
 package com.example.hexagon_tecnico.presentation.inactive_user.components
 
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
