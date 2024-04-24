aec56d0177f54e89b7d9dcdd14d22f8e9ee62eb0
Pedro H Borba <pedroborba005@gmail.com>
2024-04-23 22:19:51 -0300

    MainScreen

--- app/src/main/java/com/example/hexagon_tecnico/presentation/MainActivity.kt
+++ app/src/main/java/com/example/hexagon_tecnico/presentation/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.hexagon_tecnico.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.hexagon_tecnico.navigation.BottomNavigationBar
 import com.example.hexagon_tecnico.navigation.NavGraph
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,9 +19,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavGraph(
-                navController = rememberNavController()
-            )
+            MainScreen()
         }
+    }
+}
+
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) { innerPadding ->
+        NavGraph(navController = navController, modifier = Modifier.padding(innerPadding))
     }
 }
