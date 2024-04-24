8bdc56cf601357d65347e01a6688eb580b43098d
Pedro H Borba <pedroborba005@gmail.com>
2024-04-23 22:22:50 -0300

    Melhorando Text dos cards

--- app/src/main/java/com/example/hexagon_tecnico/presentation/users/components/TextAge.kt
+++ app/src/main/java/com/example/hexagon_tecnico/presentation/users/components/TextAge.kt
@@ -1,17 +1,20 @@
 package com.example.hexagon_tecnico.presentation.users.components
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun TextAuthor(
-    userAge: String
+fun TextAge(
+    userAge: String, style: TextStyle = MaterialTheme.typography.body1
 ) {
     Text(
         text = "Idade: $userAge",
+        style = style,
         color = Color.DarkGray,
         fontSize = 12.sp,
         textDecoration = TextDecoration.Underline
--- app/src/main/java/com/example/hexagon_tecnico/presentation/users/components/TextName.kt
+++ app/src/main/java/com/example/hexagon_tecnico/presentation/users/components/TextName.kt
@@ -1,16 +1,19 @@
 package com.example.hexagon_tecnico.presentation.users.components
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun TextTitle(
-    userName: String
+fun TextName(
+    userName: String, style: TextStyle = MaterialTheme.typography.subtitle1
 ) {
     Text(
         text = userName,
+        style = style,
         color = Color.DarkGray,
         fontSize = 25.sp
     )
